805ff40c3981ff5c03dbf3b6e03fa2cdce1adaed
J_max <58228107+MaximWDev4@users.noreply.github.com>
2021-05-08 16:07:51 +0600

    update angular

--- frontend/src/app/components/spiker/spiker.component.spec.ts
+++ frontend/src/app/components/spiker/spiker.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SpikerComponent } from './spiker.component';
 
@@ -6,7 +6,7 @@ describe('SpikerComponent', () => {
   let component: SpikerComponent;
   let fixture: ComponentFixture<SpikerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SpikerComponent ]
     })
--- frontend/src/app/views/webinar-room/webinar-room.component.spec.ts
+++ frontend/src/app/views/webinar-room/webinar-room.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WebinarRoomComponent } from './webinar-room.component';
 
@@ -6,7 +6,7 @@ describe('WebinarRoomComponent', () => {
   let component: WebinarRoomComponent;
   let fixture: ComponentFixture<WebinarRoomComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WebinarRoomComponent ]
     })
